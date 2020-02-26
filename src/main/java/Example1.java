3b6be0ef1241d923184766837da023037b20490f
Daniel Obot <danobot@hotmail.com>
2020-02-26 16:51:53 -0600

    Added example for students.

---
+++ src/main/java/Example1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+public class Example1 {
+    public static void main(String[] args) throws IOException {
+
+        /*
+        Start your work here.
+         */
+
+    }
+}
